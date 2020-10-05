edea49a18c8c2094983749bbb3e4de7d0b623630
Sven <s.wontroba@gmail.com>
2020-10-05 14:02:00 +0200

    fire filter for df

--- data_exploration.py
+++ data_exploration.py
@@ -7,6 +7,6 @@ This is a temporary script file.
 
 import pandas as pd
 
-df = pd.read_csv("../disaster_tweets.csv")
+df = pd.read_csv("./data/disaster_tweets.csv")
 
 print(df[df.text.str.contains('fire')])
